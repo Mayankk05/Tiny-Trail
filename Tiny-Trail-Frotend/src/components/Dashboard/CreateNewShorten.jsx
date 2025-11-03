19e67290fdde233a9e6abfab2ff1db721ea671a8
Mayankk05 <142004365+Mayankk05@users.noreply.github.com>
2025-11-03 12:30:28 +0530

    Update CreateNewShorten.jsx

--- Tiny-Trail-Frotend/src/components/Dashboard/CreateNewShorten.jsx
+++ Tiny-Trail-Frotend/src/components/Dashboard/CreateNewShorten.jsx
@@ -44,7 +44,6 @@ const CreateNewShorten = ({ setOpen, refetch }) => {
             });
           });
 
-          // await refetch();
           reset();
           setOpen(false);
     } catch (error) {
